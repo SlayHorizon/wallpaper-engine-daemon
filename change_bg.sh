7f3c15d1c9fba5d9ba00ca1749aa001916c42dde
SlayHorizon <143455174+SlayHorizon@users.noreply.github.com>
2023-12-15 18:42:27 +0100

    first commit

---
+++ change_bg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+folder=$"/home/atreus/Pictures/wallpapers"
+curr_pic=$(basename $(gsettings get org.gnome.desktop.background picture-uri-dark) | sed 's/.$//')
+pic=$(ls $folder/* | grep -v $curr_pic | shuf -n1)
+
+# picture options:
+# 'none', 'wallpaper', 'centered', 'scaled', 'stretched', 'zoom', 'spanned'.
+gsettings set org.gnome.desktop.background picture-options 'scaled'
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$pic"
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	while (1)
+	{
+		system("sh /home/atreus/Documents/wallpaper_engine/change_bg.sh");
+		sleep(20);
+	}
+	return (0);
+}
